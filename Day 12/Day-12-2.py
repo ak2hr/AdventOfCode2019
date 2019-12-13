fb17d35995bfe6341f2e16bde3ee99276db13aaf
Andrew Kepley <ak2hr@virginia.edu>
2019-12-12 19:09:32 -0500

    completed day 12 part 2

--- Day 12/Day-12-2.py
+++ Day 12/Day-12-2.py
@@ -1,9 +1,14 @@
+import copy
 
 poses = [
     [0,0,0],[0,0,0],[0,0,0],[0,0,0]
 ]
 
 velocities = [
+    [0,0,0],[0,0,0],[0,0,0],[0,0,0]
+]
+
+allZeroes = [
     [0,0,0],[0,0,0],[0,0,0],[0,0,0]
 ]
 
@@ -23,7 +28,7 @@ def getNewPosition(moon):
     for x in range(3):
         poses[moon][x] += velocities[moon][x]
 
-file = open("C:/Users/akepley/Documents/AdventOfCode2019/Day 12/test2.txt", "r")
+file = open("C:/Users/Andrew/Documents/Developer Stuff/AdventOfCode2019/Day 12/input.txt", "r")
 lines = file.readlines()
 
 
@@ -32,18 +37,23 @@ for x in range(4):
     for y in range(3):
         poses[x][y] = int(tempLine[y][tempLine[y].find("=") + 1:])
 
+originals = copy.deepcopy(poses)
+
 steps = 0
 mySet = {0}
 mySet.remove(0)
-#while(True):
-for x in range(1000000):
+while(True):
     for a in range(4):
         getNewVelocity(a)
     for a in range(4):
         getNewPosition(a)
-    if(str(poses + velocities) in mySet):
-        break
-    else:
-        mySet.add(str(poses+velocities))
+    # if(str(poses + velocities) in mySet):
+    #     break
+    # else:
+    #     mySet.add(str(poses+velocities))
+    x = 0
+    if(poses[0][x] == originals[0][x] and poses[1][x] == originals[1][x] and poses[2][x] == originals[2][x] and poses[3][x] == originals[3][x]):
+        print(steps, ", ", poses, "Velocities: ", velocities)
+        steps = 0
     steps += 1
 print(steps)
