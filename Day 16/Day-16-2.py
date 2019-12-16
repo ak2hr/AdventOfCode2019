e121a0e250f34f5ebe1b417388a8695bf167ea72
Andrew Kepley <akepley@c20g.com>
2019-12-16 16:56:23 -0500

    cleaned up Day 16 part 2 template

--- Day 16/Day-16-2.py
+++ Day 16/Day-16-2.py
@@ -1,6 +1,6 @@
 import copy, math
 
-file = open("Day 16/input.txt", "r")
+file = open("Day 16/test4.txt", "r")
 contents = file.read()
 contents = contents * 10000
 numList =  list(contents)
@@ -8,16 +8,8 @@ pattern = [0,1,0,-1]
 nextPhase = copy.deepcopy(numList)
 size = len(numList)
 for q in range(100):
-    currentPat = 0
     for x in range(1, size + 1):
-        total = 0
-        currentPat = 0
-        for y in range(1, size + 1):
-            if(y % x == 0):
-                currentPat += 1
-            patternVal = pattern[currentPat % 4]
-            addVal = int(numList[y-1]) * patternVal
-            total += addVal
+        
         nextPhase[x-1] = abs(total) % 10
     numList = copy.deepcopy(nextPhase)
     print(q)
