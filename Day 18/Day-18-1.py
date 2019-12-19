752b6ab5eda90c080509602b9a92affda32da4a8
Andrew Kepley <akepley@c20g.com>
2019-12-19 16:48:39 -0500

    Completed Day 18 Part 1

--- Day 18/Day-18-1.py
+++ Day 18/Day-18-1.py
@@ -1,57 +1,84 @@
 import copy
 
 discovered = {}
+start = (0,0)
+minimums = {}
+ret = {}
 
 def findAllKeys(curMap, curPos, steps):
-    ret = {0}
-    ret.remove(0)
+    global ret
     x =  curPos[1]
     y = curPos[0]
-    # print((x,y), curMap[y][x], ord(curMap[y][x]))
-    discovered[curPos] = 0
+    # if(curPos in discovered):
+    #     if(steps < discovered[curPos]):
+    #         discovered[curPos] = steps
+    # else:
+    #     discovered[curPos] = steps
     if(curMap[y][x] != '#' and (ord(curMap[y][x]) < 65 or ord(curMap[y][x]) > 90)):
         if(97 <= ord(curMap[y][x]) <= 122):
-            ret.add((curMap[y][x]))
+            if(curMap[y][x] in ret):
+                if(steps < ret[curMap[y][x]]):
+                    ret[curMap[y][x]] = steps
+            else:
+                ret[curMap[y][x]] = steps
         else:
             steps += 1
-            if((y+1, x) not in discovered):
-                ret = ret|findAllKeys(curMap, (y+1, x), steps)
-            if((y-1, x) not in discovered):
-                ret = ret|findAllKeys(curMap, (y-1, x), steps)
-            if((y, x+1) not in discovered):
-                ret = ret|findAllKeys(curMap, (y, x+1), steps)
-            if((y, x-1) not in discovered):
-                ret = ret|findAllKeys(curMap, (y, x-1), steps)
-    return ret
+            if((y+1, x) not in discovered or discovered[(y+1,x)] > steps):
+                discovered[(y+1,x)] = steps
+                findAllKeys(curMap, (y+1, x), steps)
+            if((y-1, x) not in discovered or discovered[(y-1,x)] > steps):
+                discovered[(y-1,x)] = steps
+                findAllKeys(curMap, (y-1, x), steps)
+            if((y, x+1) not in discovered or discovered[(y,x+1)] > steps):
+                discovered[(y,x+1)] = steps
+                findAllKeys(curMap, (y, x+1), steps)
+            if((y, x-1) not in discovered or discovered[(y,x-1)] > steps):
+                discovered[(y,x-1)] = steps
+                findAllKeys(curMap, (y, x-1), steps)
 
 
 def collectKey(key, thisMap):
     door = key.upper()
-    for y in thisMap:
-        for x in thisMap:
-            if(thisMap[y][x] == key or thisMap[y][x] == door):
+    newPos = start
+    for y in range(len(thisMap)):
+        for x in range(len(thisMap[y])):
+            if(thisMap[y][x] == key):
                 thisMap[y][x] = '.'
-    return (thisMap, (y,x))
+                newPos = (y,x)
+            if(thisMap[y][x] == door):
+                thisMap[y][x] = '.'
+    return (thisMap, newPos)
 
 
 
-def getMinimumSteps(remove, curMap, curPos):
-    curMap = collectKey(remove, curMap)[0] 
+def getMinimumSteps(remove, curMap, curPos, recurse):
+    global minimums
+    global ret
+    recurse += 1
+    keyResult = collectKey(remove, curMap)
+    curMap = keyResult[0]
+    curPos = keyResult[1] 
     discovered.clear()
-    keys = findAllKeys(curMap, curPos, 0)
-    if(len(keys) == 0):
-        return 0
-    else:
-        minimum = 10000
-        for x in keys:
-            val = getMinimumSteps(x, curMap, curPos)
-            if(val < minimum):
-                minimum = val
-    return val
+    ret = {}
+    findAllKeys(curMap, curPos, 0)
+    keys = copy.deepcopy(ret)
+    if(recurse < 20):
+        print(recurse, remove, keys)
+    if(str(curPos)+str(keys) not in minimums):
+        minimum = 100000
+        if(len(keys) > 0):
+            for x in copy.deepcopy(keys):
+                val = keys[x] + getMinimumSteps(x, copy.deepcopy(curMap), copy.deepcopy(curPos), recurse)
+                if(val < minimum):
+                    minimum = val
+            minimums[str(curPos)+str(keys)] = minimum
+        else:
+            minimums[str(curPos)+str(keys)] = 0
+    return minimums[str(curPos)+str(keys)]
 
 
 
-file = open("Day 18/test5.txt", 'r')
+file = open("Day 18/input.txt", 'r')
 lines = file.readlines()
 origMap = []
 for x in lines:
@@ -61,9 +88,8 @@ for x in lines:
     origMap.append(temp)
 width = len(origMap[0])
 height = len(origMap)
-start = (0,0)
 for y in range(height):
     for x in range(width):
         if(origMap[y][x] == '@'):
             start = (y,x)
-getMinimumSteps('', origMap, start)
+print(getMinimumSteps('', copy.deepcopy(origMap), start, 0))
